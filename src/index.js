a1b1e5204b9b795433fe0ff24f4b7d42c8466678
Nodreame <nodreame@163.com>
2018-10-23 19:40:35 +0800

    feat(project): add index.html & index.js with implicit dependencies

---
+++ src/index.js
@@ -0,0 +1,6 @@
+function component () {
+    let element = document.createElement('div');
+    element.innerHTML = _.join(['Hello', 'Webpack'], '');
+    return element;
+}
+document.body.appendChild(component());
