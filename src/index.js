5bc3ee9141f47717c0cfd14f10da291162e3905b
Nodreame <nodreame@163.com>
2018-10-25 11:13:01 +0800

    feat(project): finish loaders study

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import _ from 'lodash';
 import printMe from './print';
 import './styles.css';
+import JinJpg from './jin.jpg';
+import data from './data.xml';
 function component () {
+    console.log('data:', data);
     let element = document.createElement('div');
     let btn = document.createElement('button');
     element.innerHTML = _.join(['Hello', 'Webpack'], ' ');
+    element.classList.add('test');
     btn.innerHTML = 'Click me and check the console.';
     btn.onclick = printMe;
     element.appendChild(btn);
+
+    let image = new Image();
+    image.src = JinJpg;
+    element.appendChild(image);
     return element;
 }
 // document.body.appendChild(component());
